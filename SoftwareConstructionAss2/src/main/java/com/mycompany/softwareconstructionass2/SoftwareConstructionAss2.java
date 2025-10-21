a5469058dbfe69fb77eac538bec8e49858cc41e6
Sean Day <xmq6738@autuni.ac.nz>
2025-10-21 20:51:53 +1300

    testing commits

---
+++ SoftwareConstructionAss2/src/main/java/com/mycompany/softwareconstructionass2/SoftwareConstructionAss2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.softwareconstructionass2;
+
+/**
+ *
+ * @author GGPC
+ */
+public class SoftwareConstructionAss2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
